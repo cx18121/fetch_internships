6b961f2a5e13c72f9ebf9fc5dc8f2a2dfa2cb43b
cx18121 <charlie.l.xue@gmail.com>
2026-03-17 12:54:00 -0400

    update bash script

--- run_daily.sh
+++ run_daily.sh
@@ -5,4 +5,4 @@
 TO_EMAIL="charlie.l.xue@gmail.com"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-python3 "$SCRIPT_DIR/fetch.py" --email "$TO_EMAIL"
+python3 "$SCRIPT_DIR/fetch.py" -d 1 --email "$TO_EMAIL"
